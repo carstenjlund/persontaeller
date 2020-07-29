3fb2a9ffa3f93ee56cfc8c8ba44b78c342efe29d
clurts <clu@rts.dk>
2020-07-29 18:02:16 +0200

    add splash screen automation

---
+++ scripts/splash.js
@@ -0,0 +1,10 @@
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("loaded");
+
+    setTimeout(function () {
+        window.location.replace("/taeller.html")
+    }, 2500);
+})

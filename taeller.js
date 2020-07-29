66efdfac9c0603d3b780c568013f65fc0765428f
clurts <clu@rts.dk>
2020-07-29 17:47:25 +0200

    initial commit

---
+++ taeller.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const db = firebase.firestore();
+    const countElm = document.querySelector(".currentCount")
+    const addElm = document.querySelector(".addOne")
+    const subElm = document.querySelector(".subOne")
+    const docRef = db.collection("persons").doc("ydWHhS0Mas7xyWvCfSET");
+
+
+    docRef.onSnapshot({
+        // Listen for document metadata changes
+        includeMetadataChanges: true
+    }, function (doc) {
+        countElm.textContent = doc.data().count;
+    });
+
+    docRef.get().then(function (doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+
+
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function (error) {
+        console.log("Error getting document:", error);
+    });
+
+    addElm.addEventListener('click', function () {
+        docRef.update({
+            count: firebase.firestore.FieldValue.increment(1)
+        })
+    })
+    subElm.addEventListener('click', function () {
+        docRef.update({
+            count: firebase.firestore.FieldValue.increment(-1)
+        })
+    })
+
+})

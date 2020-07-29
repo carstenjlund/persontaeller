96466d36b110a133a8c5ddc4a3da65cf2084fac1
clurts <clu@rts.dk>
2020-07-29 18:09:11 +0200

    update splash script

--- scripts/splash.js
+++ scripts/splash.js
@@ -5,6 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("loaded");
 
     setTimeout(function () {
-        window.location.replace("/taeller.html")
+        window.location.replace("./taeller.html")
     }, 2500);
 })
